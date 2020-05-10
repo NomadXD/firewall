25b547862f66b26cc523d81115c8c2b5d34c8f5c
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-05-10 21:23:50 +0530

    final before upload

--- app.py
+++ app.py
@@ -1,46 +1,52 @@
-
-
-
 def get_ip_header(frame):
+    """ Extract ip header from a frame(a hex stream)
+        Frame is obtained by reading the text file and stripping white space.
+        Method returns a object that contain IP header information.
+    """
     ip_header = {}
     ip_header['version'] = int(frame[28:29],16)
     ip_header['header_length'] = int(frame[29:30],16)
     ip_header['length'] = int(frame[32:36],16)
     ip_header['ttl'] = int(frame[44:46],16)
     ip_header['protocol'] = int(frame[46:48],16)
-    ip_header['source_address'] = [int(frame[52:54],16),int(frame[54:56],16),int(frame[56:58],16),int(frame[58:60],16)]
-    ip_header['destination_address'] = [int(frame[60:62],16),int(frame[62:64],16),int(frame[64:66],16),int(frame[66:68],16)]
-    #print(ip_header['protocol'])
-    #print(ip_header['source_address'])
-    #print(ip_header['destination_address'])
-    #print(frame[56:58])
-    #print(ip_header['version'])
-    #print(ip_header['length'])
-    #print(ip_header['ttl'])
-    #print(ip_header['protocol'])
-    print(ip_header)
+    ip_src = [int(frame[52:54],16),int(frame[54:56],16),int(frame[56:58],16),int(frame[58:60],16)]
+    ip_header['source_address'] = '.'.join([str(x) for x in ip_src])
+    ip_dest = [int(frame[60:62],16),int(frame[62:64],16),int(frame[64:66],16),int(frame[66:68],16)]
+    ip_header['destination_address'] = '.'.join([str(x) for x in ip_dest])
+    
+    return ip_header
 
 
 def get_tcp_header(frame):
+    """Extract tcp header information from a frame(a hex stream)
+        Frame is obtained by reading the text file and stripping white space.
+        Method returns a object that contain TCP header information.
+    """
     tcp_header = {}
     tcp_header['source_port'] = int(frame[68:72],16)
     tcp_header['destination_port'] = int(frame[72:76],16)
-    print(frame[72:76])
+    
+    return tcp_header
 
 
 def read_frames(filename):
+    """Method to read a text file line by line and store values
+        in an array.
+    """
     with open(filename) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
-    print(content)
+    for frame in content:
+        get_ip_header(frame)
+        get_tcp_header(frame)
     return content
-    #for frame in content:
-        #get_ip_header(frame)
-        #get_tcp_header(frame)
-    #print(content)
 
 
 def read_rules(filename):
+    """ Read a config file and parse the
+        rules stored in it. Stores the rules 
+        in an array.
+    """
     rules = []
     with open(filename) as f:
         content = f.readlines()
@@ -50,28 +56,49 @@ def read_rules(filename):
         if(line and not line.startswith("#")):
             line = line.split('\t\t\t')
             rules.append(line)
+    return rules
 
-    print(rules)
 
-
-def simulate_int_one():
-    frames = read_frames('interface_external.txt')
+def simulate_firewall(interface):
+    if(interface == 'external'):
+        frames = read_frames('interface_external.txt')
+    else:
+        frames = read_frames('interface_internal.txt')
+    rules = read_rules('config.txt')
     for frame in frames:
-        print(frame)
+        accepted = False
+        ip_header = get_ip_header(frame)
+        tcp_header = get_tcp_header(frame)
+        print(ip_header,tcp_header)
+        for rule in rules:
+            if(rule[1] == 'TCP' and ip_header['protocol'] == 6 and rule[6] == '1'):
+                if((rule[2] == 'anywhere' or ip_header['source_address'] == rule[2]) and 
+                    (str(tcp_header['destination_port']) == 'any' or str(tcp_header['destination_port']) == rule[5])):
+                    if(rule[0] == 'ACCEPT'):
+                        accepted = True
+                        print("Packet accepted under rule {0}".format(rule[7]))
+                    else:
+                        accepted = False
+                        print("Packet dropped under rule {0}".format(rule[7]))
+                    
+            elif(rule[1] == 'UDP' and ip_header['protocol'] == 17 and rule[6] == '1'):
+                if((rule[2] == 'anywhere' or ip_header['source_address'] == rule[2]) and 
+                    (str(tcp_header['destination_port']) == 'any' or str(tcp_header['destination_port']) == rule[5])):
+                    if(rule[0] == 'ACCEPT'):
+                        accepted = True
+                        print("Packet accepted under rule %s" %rule[7])
+                    else:
+                        accepted = False
+                        print("Packet dropped under rule %s" %rule[7])
 
+        if(accepted):
+            print("Forwarding packet to {0}".format(ip_header["destination_address"]))
+        else:
+            print("Packet dropped.")
         
-        
+        #print(accepted)
 
 
-#simulate_int_one()
-
-#read_frames('interface_external.txt')
-
-#read_rules('config.txt')
-#read_frames('interface_external.txt')
+simulate_firewall('external')
 
 
-
-# def get_ip_header(frame):
-#     ip_header = {}
-#     ip_header['version'] = int(frame[])
